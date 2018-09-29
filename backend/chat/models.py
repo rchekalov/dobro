6b118d38e9df4c98d8b99689b775fc81d516625e
rchekalov <rchekalov@me.com>
2018-09-29 17:28:46 +0300

    add migrations

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Dialog(models.Model):
+    uuid = models.UUIDField(primary_key=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class Message(models.Model):
+    dialog = models.ForeignKey(Dialog, on_delete=True)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class Snippet(models.Model):
+    answer = models.TextField()
+    question = models.TextField()
